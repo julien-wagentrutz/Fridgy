bece1fe3767650d14eb301cce9dc47129702f740
julien Wagentrutz <julien.wagentrutz@hetic.net>
2019-12-18 11:48:12 +0100

    Add quantity and img into bd from product

--- Treatment/addProductToBD.php
+++ Treatment/addProductToBD.php
@@ -6,8 +6,11 @@
     $codeBar = $_POST['codeBar'];
     $productBrand = $_POST['brand'];
     $productName = $_POST['name'];
+    $productImg = $_POST['imgSrc'];
+    $productQuantiy = $_POST['productQuantity'];
 
-    $query = "INSERT INTO productScan(code_bar,user ,product_brand, product_name) VALUES (". $codeBar . ",'".$user ."','".$productBrand."','".$productName."')";
+    $query = "INSERT INTO productScan(code_bar,user ,product_brand, product_name, img_src,product_quantity) VALUES (". $codeBar . ",'".$user ."','".$productBrand."','".$productName."','". $productImg ."','". $productQuantiy ."')";
+    echo $query;
     $mysqli->query($query);
 
 
--- include/productCard.php
+++ include/productCard.php
@@ -1,10 +1,10 @@
 <div class="productCard">
-    <img src="../Images/innocent.png" alt="innocent" class="productImage">
+    <img src= <?php echo  $product['img_src'] ?> alt="innocent" class="productImage">
     <div class="cardContain">
         <div class="productInformation">
             <h2 class="productName"><?php echo  $product['product_name'] ?></h2>
             <h3 class="productBrand"><?php echo $product['product_brand'] ?></h3>
-            <h3 class="productQuantity">1L</h3>
+            <h3 class="productQuantity"><?php echo $product['product_quantity'] ?></h3>
             <span class="expiryDate">11.12.20</span>
             <span class="daysLeft">2 jours</span>
             <div class="timeBar">
